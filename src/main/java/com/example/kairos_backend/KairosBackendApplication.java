53e029dbb49a6d95647ffbff92a983d12f711d90
Terry Gao <cngao412@gmail.com>
2023-02-21 16:04:01 -0800

    init project

---
+++ src/main/java/com/example/kairos_backend/KairosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kairos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KairosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KairosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kairos_backend/KairosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kairos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KairosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
