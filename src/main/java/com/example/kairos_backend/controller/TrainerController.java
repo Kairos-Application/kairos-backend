c00499cd31be408beebf127838ea146fdac772b3
Terry Gao <cngao412@gmail.com>
2023-02-24 13:22:48 -0800

    trainer CRUD

--- src/main/java/com/example/kairos_backend/controller/TrainerController.java
+++ src/main/java/com/example/kairos_backend/controller/TrainerController.java
@@ -3,9 +3,7 @@ package com.example.kairos_backend.controller;
 import com.example.kairos_backend.service.TrainerService;
 import com.example.kairos_backend.entity.Trainer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +18,26 @@ public class TrainerController {
     }
 
     @GetMapping
-    public List<Trainer> getTrainer(){
-        return trainerService.getTrainer();
+    public List<Trainer> getTrainers(){
+        return trainerService.getTrainers();
+    }
+
+    @PostMapping
+    public void saveTrainer(@RequestBody Trainer trainer){
+        trainerService.saveTrainer(trainer);
+    }
+
+    @DeleteMapping(path = "{trainerId}")
+    public void delTrainer(@PathVariable("trainerId") Long trainerId){
+        trainerService.delTrainer(trainerId);
+    }
+
+    @PutMapping(path = "{trainerId}")
+    public void updateTrainer(
+            @PathVariable("trainerId") Long trainerId,
+            @RequestParam(required = true) String name,
+            @RequestParam(required = true) String email
+    ){
+        trainerService.updateTrainer(trainerId, name, email);
     }
 }
